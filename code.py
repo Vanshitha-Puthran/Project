66c912a078386208743b6ce28d18e0e6a05bae3f
Vanshitha <vanshithaputhran@gamil.com>
2024-09-09 15:53:56 +0530

    Deleted

--- code.py
+++
@@ -1,5 +0,0 @@
-_,axes=plt.subplots(nrows=1,ncols=4,figsize=(10,3))
-for ax,image,label in zip(axes,df.images,df.target):
-  ax.set_axis_off()
-  ax.imshow(image,cmap=plt.cm.gray_r,interpolation="nearest")
-  ax.set_title("Training:%i"% label)
